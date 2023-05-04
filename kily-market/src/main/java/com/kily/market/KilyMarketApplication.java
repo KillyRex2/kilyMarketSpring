55d82ea7b6644efa5e84560513e3ac79c94896e0
KillyRex2 <anggelarcce09@gmail.com>
2023-05-04 13:09:54 -0600

    ft commit

---
+++ kily-market/src/main/java/com/kily/market/KilyMarketApplication.java
@@ -0,0 +1,13 @@
+package com.kily.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KilyMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KilyMarketApplication.class, args);
+	}
+
+}
---
+++ kily-market/src/test/java/com/kily/market/KilyMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kily.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KilyMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
